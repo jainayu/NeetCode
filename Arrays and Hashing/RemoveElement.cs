f0bf16c4954550ec9b7e4884b143500eb2b2b141
jainayu <ayujain.728@gmail.com>
2022-10-08 14:05:16 +0530

    optimizing remove element code

--- Arrays and Hashing/RemoveElement.cs
+++ Arrays and Hashing/RemoveElement.cs
@@ -1,15 +1,14 @@
 public class Solution {
     public int RemoveElement(int[] nums, int val) {
-        int count = 0; 
+        int k = 0; 
+        
         for(int i = 0; i < nums.Length; i++){
-            if(nums[i] == val){
-                count++;
-                nums[i] = Int32.MaxValue;
+            if(nums[i] != val){
+                nums[k] = nums[i];
+                k++;
             }
         }
-        Array.Sort(nums);
-        
-        return nums.Length - count;
-        
+
+        return k;
     }
 }
